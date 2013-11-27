25d193365fb39dc029514f5c80a7ffcb281af7ac
matsukawa <mtkwsny.sunrize@gmail.com>
2013-11-27 18:40:02 +0900

    日付をフォーマットした。
    スレッドタイトル一覧ページ、エントリー一覧表示ページに表示されている
    日時を日本語表示にフォーマットした。
    http://board.sunrisedigital.jp/thread3/6/list
    http://board.sunrisedigital.jp/entry3/5/list

--- board.sunrisedigital.jp/app/controllers/default/ControlController.php
+++ board.sunrisedigital.jp/app/controllers/default/ControlController.php
@@ -7,7 +7,13 @@ class ControlController extends Sdx_Controller_Action_Http{
     }
     
     public function threadAction(){
-        
+        $this->_helper->scaffold->setViewRendererPath('default/control/scaffold.tpl');
+        $this->_helper->scaffold->run();
+    }
+    
+    public function genreAction(){
+        $this->_helper->scaffold->setViewRendererPath('default/control/scaffold.tpl');
+        $this->_helper->scaffold->run();
     }
     
 }
--- common/models/Bd/Orm/Main/Table/Thread.php
+++ common/models/Bd/Orm/Main/Table/Thread.php
@@ -2,9 +2,16 @@
 
 require_once 'Bd/Orm/Main/Base/Table/Thread.php';
 
-class Bd_Orm_Main_Table_Thread extends Bd_Orm_Main_Base_Table_Thread
-{
+class Bd_Orm_Main_Table_Thread extends Bd_Orm_Main_Base_Table_Thread {
 
+    public function fetchAllNewerOrdered(Sdx_Db_Select $select = null) {
+        if ($select === null) {
+            $select = $this->getSelect();
+        }
+
+        $select->order('id DESC');
+
+        return $this->fetchAll($select);
+    }
 
 }
-
