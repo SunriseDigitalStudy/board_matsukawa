f4194d5530ca6eb57b955c70fd9a923c70bf6e49
matsukawa <mtkwsny.sunrize@gmail.com>
2014-01-16 11:26:56 +0900

    Ajax通信で検索処理を実行するコントローラとテンプレートを新規作成
    とりあえず、通信はうまくいったのでコミット。

---
+++ board.sunrisedigital.jp/app/controllers/default/AjaxController.php
@@ -0,0 +1,223 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AjaxController
+ *
+ * @author Matsukawa
+ */
+class AjaxController extends Sdx_Controller_Action_Http{
+  
+  public function indexAction() {
+    $this->_disableViewRenderer();
+  }
+
+ public function listAction() {
+
+
+    /*
+     * Sdx_Formで入力フォームを作成
+     */
+
+    $form = new Sdx_Form();
+    $form
+            ->setAction("javascript:void(0);") //Action先を現在のURLに指定
+            ->setMethodToPost()  //メソッドをPOSTに変更
+            ->setId('form1');
+            
+    //各フォームにエレメントをセット
+    //ジャンル選択ラジオボタン
+    $elems = new Sdx_Form_Element_Group_Radio();
+    $genre_list = Bd_Orm_Main_Genre::createTable()
+            ->getSelect()
+            ->setColumns(array('id', 'name'))
+            ->fetchPairs();    
+    $elems->setName('genre_id')->addChildren($genre_list);    
+    $form->setElement($elems);
+
+    //タグ選択チェックボックス
+    $elems = new Sdx_Form_Element_Group_Checkbox();
+    $tag_list = Bd_Orm_Main_Tag::createTable()
+            ->getselect()
+            ->setColumns(array('id', 'name'))
+            ->fetchPairs();
+    $elems->setName('tag_ids')->addChildren($tag_list);
+    $form->setElement($elems);
+    
+    
+    /*
+     * 選択された値(チェック)が他ページから遷移してきた時に反映されるようにする処理
+     * search/listから飛んで来た場合とentry3/listから飛んで来た場合で条件分岐
+     */
+    //条件分岐するために、リンク元のURLを取得
+    
+    if ($_SERVER['HTTP_REFERER']) {
+      $referer_url = parse_url($_SERVER['HTTP_REFERER']);
+      $referer_path = $referer_url['path'];
+      //$referer_pathはカスタムルートのため、条件分岐できるように、文字列'entry3'だけを抜き取る。
+      $entry3 = substr($referer_path, 1, 6);
+    }
+    
+    //sessionにパラメータを保存。entryページからのリンクの場合はsessionに値が上書きされないようにする。
+    $session = new Zend_Session_Namespace('search');
+    if('entry3' !== $entry3){
+      $session->param = $this->_getAllParams();
+    }
+
+    //パラメータを入力フォームにバインド
+    if('entry3' == $entry3){
+      $form->bind($session->param);
+    }
+
+    $this->view->assign('form', $form);
+
+
+
+    /* -------------------------------------------------------------------------------
+     *  複合条件検索のSQL文
+      SELECT *
+      FROM thread
+      INNER JOIN thread_tag ON thread.id = thread_tag.thread_id
+      INNER JOIN tag ON thread_tag.tag_id = tag.id
+      WHERE
+      thread_tag.tag_id IN (11, 10)
+      AND
+      genre_id = 4
+      GROUP BY
+      thread.id HAVING COUNT(tag_id) = 2; //2はチェックボックスのチェックされた数
+     * ------------------------------------------------------------------------------
+     */
+
+    /*
+     * 複合条件検索のSQLをORMで作成
+     */
+
+    //絞り込み条件の値(パラメータ)を取得
+    $genre_id = $this->_getParam('genre_id');
+    $tag_ids = $this->_getParam('tag_ids');
+
+    //並び順用サブクエリの作成
+    //SELECT thread_id, Max(updated_at) AS updated  FROM entry GROUP BY thread_id           
+    $t_entry = Bd_Orm_Main_Entry::createTable();
+    $select_en = $t_entry->getSelect();
+    $select_en->resetColumns()
+            ->columns('thread_id')
+            ->columns('Max(updated_at) AS updated')
+            ->group('thread_id');
+
+
+    //メインクエリ作成
+    $t_thread = Bd_Orm_Main_Thread::createTable();
+
+    //タグ条件絞り込み検索のためのテーブル生成＆join
+    if ($tag_ids) {
+      $t_thread_tag = Bd_Orm_Main_ThreadTag::createTable();
+      $t_thread->addJoinLeft($t_thread_tag);
+    }
+
+    //全件検索
+    $select_th = $t_thread->getSelectWithJoin();
+    $sub_Query = $select_th->expr('(' . $select_en->assemble() . ')');
+    $select_th
+            ->joinLeft(array('max_updated' => $sub_Query), 'thread.id = max_updated.thread_id')
+            ->order('(CASE WHEN updated is null THEN 1 ELSE 2 END), updated DESC');
+    //タグ条件で絞込み
+    if ($tag_ids) {
+      $select_th
+              ->add('thread_tag.tag_id', $tag_ids)
+              ->group('thread.id')
+              ->having('COUNT(tag_id) =' . count($tag_ids));
+    }
+    //ジャンル条件で絞込み
+    if ($genre_id) {
+      $select_th
+              ->add('genre_id', $genre_id);
+    }
+
+    //sql発行
+    $thread_list = $t_thread->fetchAll($select_th);
+    //テンプレにアサイン
+    $this->view->assign('thread_list', $thread_list);
+    
+  }
+  
+  public function searchAction(){
+    $this->_disableViewRenderer();
+    
+    $genre_id = $this->_getParam('genre_id');
+    $tag_ids = $this->_getParam('tag_ids');
+
+    //並び順用サブクエリの作成
+    //SELECT thread_id, Max(updated_at) AS updated  FROM entry GROUP BY thread_id           
+    $t_entry = Bd_Orm_Main_Entry::createTable();
+    $select_en = $t_entry->getSelect();
+    $select_en->resetColumns()
+            ->columns('thread_id')
+            ->columns('Max(updated_at) AS updated')
+            ->group('thread_id');
+
+
+    //メインクエリ作成
+    $t_thread = Bd_Orm_Main_Thread::createTable();
+
+    //タグ条件絞り込み検索のためのテーブル生成＆join
+    if ($tag_ids) {
+      $t_thread_tag = Bd_Orm_Main_ThreadTag::createTable();
+      $t_thread->addJoinLeft($t_thread_tag);
+    }
+
+    //全件検索
+    $select_th = $t_thread->getSelectWithJoin();
+    $sub_Query = $select_th->expr('(' . $select_en->assemble() . ')');
+    $select_th
+            ->joinLeft(array('max_updated' => $sub_Query), 'thread.id = max_updated.thread_id')
+            ->order('(CASE WHEN updated is null THEN 1 ELSE 2 END), updated DESC');
+    //タグ条件で絞込み
+    if ($tag_ids) {
+      $select_th
+              ->add('thread_tag.tag_id', $tag_ids)
+              ->group('thread.id')
+              ->having('COUNT(tag_id) =' . count($tag_ids));
+    }
+    //ジャンル条件で絞込み
+    if ($genre_id) {
+      $select_th
+              ->add('genre_id', $genre_id)
+              ->group('thread.id');
+    }
+
+    //sql発行
+    $thread_list = $t_thread->fetchAll($select_th);
+    Sdx_Debug::dump($thread_list);
+    
+    /*
+     * jsonオブジェクトを渡す
+     */
+//    $thread1 = array();
+//    $thread_list1 = array(); 
+//    foreach ($thread_list as $thread) {
+//      $thread1[] = $thread->getId();
+//      $thread1[] = $thread->getTitle();
+//      if($thread->get('updated')){
+//        $thread1[] = $thread->getZendDate('updated')->get('yyyy年MM月dd日(E) HH時mm分ss秒');
+//        $thread_list1[] = $thread1;
+//      } 
+//      $thread1 = array();
+//    }
+    
+    
+    echo json_encode($thread_list1);
+    Sdx_Debug::dump($thread_list->getFirstRecord()->getId(),'record');
+//    Sdx_Debug::dump($thread_list->getFirstRecord()->getZendDate('updated')->get('yyyy年MM月dd日(E) HH時mm分ss秒','record'), 'yyyyy');
+    
+    Sdx_Debug::dump($thread_list1 , '$thread_list1 ');
+    Sdx_Debug::dump($thread1 , 'thread1 ');
+
+  }
+    
+}
---
+++ board.sunrisedigital.jp/app/views/default/ajax/list.tpl
@@ -0,0 +1,85 @@
+
+{extends file='default/base.tpl'}
+{block title append} Ajaxリスト{/block}
+{block main_contents}
+  <div class="row">
+    
+    <div class="col-sm-4">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3>検索フォーム</h3>
+        </div>
+        <div class="panel-body point-delete">
+          {$form->renderStartTag() nofilter}
+          <h3><span class="label label-default">ジャンル選択</span></h3>
+          {$form.genre_id->setDefaultEmptyChild('何も選択しない')->render() nofilter}
+          <h3><span class="label label-default">タグ選択</span></h3>
+          {$form.tag_ids->render() nofilter}
+          <button type="submit" class="btn btn-success"><b>検索</b><i class="glyphicon glyphicon-hand-left"></i></button>
+          <input class="btn btn-danger clearForm" type="button" value="リセット">
+          </form>
+        </div>
+      </div>
+    </div>
+          
+    <div class="col-sm-8">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3>検索結果(スレッド一覧)</h3>
+        </div>
+        <div class="panel-body">
+          {if get_class($thread_list->getFirstRecord()) == Sdx_Null }
+            <p style="font-size:200%">検索条件に一致するスレッドはありません</p><br/>
+            <p><img src="/img/20081221231807.jpg" alt="やる夫3"></p>
+          {else}
+            <ul>
+              {foreach $thread_list as $thread}
+                <li>
+                  <span style="font-size:130%"><a href="/entry3/{$thread->getId()}/list">{$thread->getTitle()}</a></span>
+                  &nbsp;
+                {if $thread->get('updated')}{$thread->getZendDate('updated')->get('yyyy年MM月dd日(E) HH時mm分ss秒')}{else}コメントは一件もありません{/if}
+              </li>        
+            {/foreach}
+          </ul>
+        {/if}
+      </div>     
+    </div>      
+      <p style="font-size:200%"><img src="/img/20110224223407740.png" alt="やる夫2">自由に書き込んだらいいお</p>     
+  </div>
+
+
+  <script>
+    $(document).ready(function(){
+      
+      $('#form1').submit(function(event) {
+        event.preventDefault(); 
+        var $form = $("#form1");
+        var formVal = $form.serialize();
+
+        $.ajax({
+          type: "GET",
+          url: "/ajax/list",
+          data: formVal,
+          success: function(data)
+          {
+            $('#content').html(data);
+          },
+          error: function(XMLHttpRequest, textStatus, errorThrown)
+          {
+            alert('Error : ' + errorThrown);
+          }
+        });
+        
+      });
+      
+    });
+    
+ 
+  $(".clearForm").bind("click", function(){
+          $(this.form).find(":checked").prop("checked", false);
+  });
+
+
+  </script>
+</div>
+{/block}
