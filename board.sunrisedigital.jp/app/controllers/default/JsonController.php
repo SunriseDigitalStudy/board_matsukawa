dc36a108a65e5cdcd48df18313475194a5fd9b6d
matsukawa <mtkwsny.sunrize@gmail.com>
2014-01-30 18:52:31 +0900

    JsonController.phpとjson/list.tplを新規作成
    AjaxController.phpとajax/list.tplは手を加えずに残しておきたかったため
    ファイルを新規作成した。

---
+++ board.sunrisedigital.jp/app/controllers/default/JsonController.php
@@ -0,0 +1,134 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AjaxController
+ *
+ * @author Matsukawa
+ */
+class JsonController extends Sdx_Controller_Action_Http{
+  
+  //ページング処理に使う値
+  const PER_PAGE = 5; //1ページあたりの表示件数
+  
+  public function indexAction() {
+    $this->_disableViewRenderer();
+  }
+
+ public function listAction() {
+
+    /*
+     * Sdx_Formで入力フォームを作成
+     */
+
+    $form = new Sdx_Form();
+    $form
+            ->setAction("javascript:void(0);") //Action先を現在のURLに指定
+            ->setMethodToPost()  //メソッドをPOSTに変更
+            ->setId('form1');
+            
+    //各フォームにエレメントをセット
+    //ジャンル選択ラジオボタン
+    $elems = new Sdx_Form_Element_Group_Radio();
+    $genre_list = Bd_Orm_Main_Genre::createTable()
+            ->getSelect()
+            ->setColumns(array('id', 'name'))
+            ->fetchPairs();
+    $elems->setName('genre_id')->addChildren($genre_list);
+    $form->setElement($elems);
+
+    //タグ選択チェックボックス
+    $elems = new Sdx_Form_Element_Group_Checkbox();
+    $tag_list = Bd_Orm_Main_Tag::createTable()
+            ->getselect()
+            ->setColumns(array('id', 'name'))
+            ->fetchPairs();
+    $elems->setName('tag_ids')->addChildren($tag_list);
+    $form->setElement($elems);
+   
+    $this->view->assign('form', $form);
+    
+  }
+  
+  public function searchAction() {
+
+    /* -------------------------------------------------------------------------------
+     *  複合条件検索のSQL文
+      SELECT *
+      FROM thread
+      INNER JOIN thread_tag ON thread.id = thread_tag.thread_id
+      INNER JOIN tag ON thread_tag.tag_id = tag.id
+      WHERE
+      thread_tag.tag_id IN (11, 10)
+      AND
+      genre_id = 4
+      GROUP BY
+      thread.id HAVING COUNT(tag_id) = 2; //2はチェックボックスのチェックされた数
+     * ------------------------------------------------------------------------------
+     */
+
+    /*
+     * 複合条件検索のSQLをORMで作成
+     */
+
+    //絞り込み条件の値(パラメータ)を取得
+    $genre_id = $this->_getParam('genre_id');
+    $tag_ids = $this->_getParam('tag_ids');
+
+    //並び順用サブクエリの作成
+    //SELECT thread_id, Max(updated_at) AS updated  FROM entry GROUP BY thread_id
+    $t_entry = Bd_Orm_Main_Entry::createTable();
+    $select_en = $t_entry->getSelect();
+    $select_en->resetColumns()
+            ->columns('thread_id')
+            ->columns('Max(updated_at) AS updated')
+            ->group('thread_id');
+
+
+    //メインクエリ作成
+    $t_thread = Bd_Orm_Main_Thread::createTable();
+
+    //タグ条件絞り込み検索のためのテーブル生成＆join
+    if ($tag_ids) {
+      $t_thread_tag = Bd_Orm_Main_ThreadTag::createTable();
+      $t_thread->addJoinLeft($t_thread_tag);
+    }
+
+    //全件検索
+    $select_th = $t_thread->getSelectWithJoin();
+    $sub_Query = $select_th->expr('(' . $select_en->assemble() . ')');
+    $select_th
+            ->joinLeft(array('max_updated' => $sub_Query), 'thread.id = max_updated.thread_id')
+            ->order('(CASE WHEN updated is null THEN 1 ELSE 2 END), updated DESC');
+    //タグ条件で絞込み
+    if ($tag_ids) {
+      $select_th
+              ->add('thread_tag.tag_id', $tag_ids)
+              ->group('thread.id')
+              ->having('COUNT(tag_id) =' . count($tag_ids));
+    }
+    //ジャンル条件で絞込み
+    if ($genre_id) {
+      $select_th
+              ->add('genre_id', $genre_id);
+    }
+    
+    //ページング処理
+    $count = $select_th->countRow(); //総レコード数
+    $sdx_pager = new Sdx_Pager(self::PER_PAGE, $count);
+    $select_th->limitPager($sdx_pager);
+
+    //sql発行
+    $thread_list = $t_thread->fetchAll($select_th);
+    //テンプレにアサイン
+    $this->view->assign('thread_list', $thread_list);
+    $this->view->assign('sdx_pager', $sdx_pager);
+    
+  }
+      
+}
---
+++ board.sunrisedigital.jp/app/views/default/json/list.tpl
@@ -0,0 +1,107 @@
+
+{extends file='default/base.tpl'}
+{block title append} Ajaxリスト{/block}
+{block main_contents}
+  <div class="row">
+
+    <div class="col-sm-4">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3>検索フォーム</h3>
+        </div>
+        <div class="panel-body point-delete">
+          {$form->renderStartTag() nofilter}
+          <h3><span class="label label-default">ジャンル選択</span></h3>
+          {$form.genre_id->setDefaultEmptyChild('何も選択しない')->render() nofilter}
+          <h3><span class="label label-default">タグ選択</span></h3>
+          {$form.tag_ids->render() nofilter}
+          <button type="submit" class="btn btn-success"><b>検索</b><i class="glyphicon glyphicon-hand-left"></i></button>
+          <input class="btn btn-danger clearForm" type="button" value="リセット">
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div id="content"></div>
+
+
+
+  </div>
+{/block}
+{block js}
+  <script>
+    $(function() {
+
+      var firstPage = 1; //表示するページのナンバー
+      ajax(firstPage);
+
+      /*
+       *検索ボタンクリックアクション
+       */
+      $('#form1').submit(function(event) {
+        //submitイベントを無効化
+        event.preventDefault();
+        //変数firstPageはレキシカル変数
+        ajax(firstPage);
+      });
+
+
+      function ajax(page) {
+        //送る値ををクエリ文字列に変換
+        var $form = $("#form1");
+        var formVal = $form.serialize();
+        formVal += '&pid=' + page;
+
+        $.ajax({
+          type: "GET",
+          url: "/ajax/search",
+          data: formVal,
+          success: function(data)
+          {
+            $("#content").html(data);
+            //出力したHTMLにクリックイベントを実装
+            initPagingEvent();
+          },
+          error: function(XMLHttpRequest, textStatus, errorThrown)
+          {
+            alert('Error : ' + errorThrown);
+          }
+        });
+        
+      }
+
+      function initPagingEvent() {
+        
+        var nextPage = Number($("#page").data('next-page'));
+        var prevPage = Number($("#page").data('prev-page'));
+        
+        //次の件数を表示
+        if (nextPage){
+          $('#next').click(function() {
+          ajax(nextPage);
+        });
+        }else{
+          $('#next').hide();
+        }
+
+        //前の件数を表示
+        if (prevPage) {
+          $('#back').click(function() {
+          ajax(prevPage);
+        });
+        }else{
+          $('#back').hide();
+        }
+        
+      }
+
+      /*
+       * 選択されたラジオボタン、チェックボックスのチェックをリセットする処理
+       */
+      $(".clearForm").bind("click", function() {
+        $(this.form).find(":checked").prop("checked", false);
+      });
+
+    });
+  </script>
+{/block}
